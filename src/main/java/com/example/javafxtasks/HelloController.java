0b94582734be6514b12ad54d05ee4ffa4a9c56e9
Arthur Mascaro <arthumascaro69@gmail.com>
2023-01-14 21:26:38 -0300

    Database integration

--- src/main/java/com/example/javafxtasks/HelloController.java
+++ src/main/java/com/example/javafxtasks/HelloController.java
@@ -1,37 +1,71 @@
 package com.example.javafxtasks;
 
+import com.example.javafxtasks.classes.bean.Product;
+import com.example.javafxtasks.classes.dao.ProductDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
-import java.time.LocalDate;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class HelloController {
 
+    @FXML private TextField priceTextField;
+    @FXML private TextField quantityTextField;
+    @FXML private TextField nameTextField;
+    @FXML private TextField codeTextField;
+    @FXML private TableView<Product> table;
+    @FXML private TableColumn<Product, Long> cCode;
+    @FXML private TableColumn<Product, String> cName;
+    @FXML private TableColumn<Product, Double> cPrice;
+    @FXML private TableColumn<Product, Integer> cQuantity;
+    private ObservableList<Product> products = FXCollections.observableArrayList();
     @FXML
-    TextField descTextField;
-    @FXML
-    TextField valueTextField;
-    @FXML
-    DatePicker datePicker;
-    @FXML
-    Button save;
-    @FXML
-    ComboBox categoryComboBox;
 
-    @FXML
     public void initialize() {
-        categoryComboBox.getItems().removeAll(categoryComboBox.getItems());
-        categoryComboBox.getItems().addAll(Product.Category.values());
-        categoryComboBox.getSelectionModel();
+        products.addAll(ProductDAO.getProducts());
+        fill();
     }
 
+    public void fill(){
+        cCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        cName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        cPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        cQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        table.setItems(products);
+    }
     public void save(){
-        String description = descTextField.getText();
-        Double value = Double.parseDouble(valueTextField.getText());
-        LocalDate date = datePicker.getValue();
-        var categoria = (Product.Category) categoryComboBox.getValue();
-        Product product = new Product(description, date, value, categoria);
-        System.out.println(product);
+        var product = new Product(Long.parseLong(codeTextField.getText()), nameTextField.getText(), Double.parseDouble(priceTextField.getText()),
+                Integer.parseInt(quantityTextField.getText()));
+        if(!ProductDAO.alreadyExists(product)) {
+            ProductDAO.save(product);
+            products.add(product);
+            fill();
+        }
+        else {
+            throw new RuntimeException("Product already exists");
+        }
+    }
+    public void delete(){
+        ProductDAO.delete(table.getSelectionModel().getSelectedItem());
+        products.remove(table.getSelectionModel().getSelectedItem());
+    }
+
+    public void update(){
+        var oldProduct = table.getSelectionModel().getSelectedItem();
+        var newProduct = new Product(Long.parseLong(codeTextField.getText()), nameTextField.getText(),
+                Double.parseDouble(priceTextField.getText()),  Integer.parseInt(quantityTextField.getText()));
+        ProductDAO.update(oldProduct, newProduct);
+        int index = products.indexOf(oldProduct);
+        products.set(index, newProduct);
+        fill();
+    }
+
+    public void clear(){
+        codeTextField.setText("");
+        nameTextField.setText("");
+        priceTextField.setText("");
+        quantityTextField.setText("");
     }
 
 }
